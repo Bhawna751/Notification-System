09a3dce84d6f115b73bdc467a2f7f531f33531ba
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2025-05-19 04:59:20 +0530

    deleted  file
    extra config redacted

--- src/main/java/com/notificationSystem/Notification/System/NotificationSystemApplication.java
+++ src/main/java/com/notificationSystem/Notification/System/NotificationSystemApplication.java
@@ -1,10 +1,19 @@
 package com.notificationSystem.Notification.System;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.retry.annotation.EnableRetry;
 
-@EnableWebSocketMessageBroker
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Notification Service API",
+				version = "1.0",
+				description = "API for sending Email, SMS, and In-App notifications"
+		)
+)
+@EnableRetry
 @SpringBootApplication
 public class NotificationSystemApplication {
 
